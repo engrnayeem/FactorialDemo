f4b66b3f81ed53e50e7f5dd7148c82070be6b944
unknown <engr.mohammadjannatulnayeem@gmail.com>
2022-10-30 01:34:53 +0600

    code uploaded

---
+++ src/FactorialDemo/FactorialDemo.java
@@ -0,0 +1,43 @@
+
+package FactorialDemo;
+
+import java.util.Scanner;
+
+
+public class FactorialDemo {
+    public static void main(String[]args)
+    {
+    
+    //variable diclararion area
+        
+      int num,i,fact=1;
+      
+      //take user input
+      
+      Scanner input=new Scanner(System.in);
+      System.out.print("Enter Any Positive Number :");
+      
+      //variable num holds user given input
+      
+      num=input.nextInt();
+      
+      for(i=num;i>=1;i--)
+      {
+      fact=fact*i;
+      
+      }
+      
+      
+      System.out.println(num+"factorial is :"+fact);
+      
+      
+      
+      
+    
+    
+    
+    
+    
+    }
+    
+}
